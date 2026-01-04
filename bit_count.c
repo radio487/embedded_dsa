c9c370a44c0fcfa47138537ed02fd7678ca0fdb5
radio487 <137423799+radio487@users.noreply.github.com>
2026-01-04 15:00:52 +0100

    Kerningham's bit counting algorithm added.

--- bit_count.c
+++ bit_count.c
@@ -1,10 +1,13 @@
 /*
- * Two variants. The first iterates all the bits by comparing with 1, and right shifting each round
- * Study the K&R method and its relation to two's complement.
+ * Two variants. The first iterates all the bits by comparing with 1, and right shifting each round. Scales linearly with the amount of bits in the int.
+ * The second is from Brian Kerningham (it can be found in a K&R exercise) and uses a clever way to set the least significant non-zero bit to zero to abort the loop in a way that scales linearly with the number of set bits.
  */
+
 #include <stdio.h>
 
-int bitcount1(unsigned int a) {
+#define SIZE 20
+
+int bitcount_naive(unsigned int a) {
   int c = 0;
   while (a) {
     if (a & 1) {
@@ -14,11 +17,22 @@ int bitcount1(unsigned int a) {
   }
   return c;
 }
+int bitcount_kerningham(unsigned int a) {
+  int c = 0;
+  while (a) {
+    c += 1;
+    a &= a - 1;
+  }
+  return c;
+}
 
 int main() {
-  int a = 8;
-  printf("%b\n", a);  
-  printf("%d\n", bitcount1(a));  
+  for (int i = 0; i < SIZE; ++i) {
+    printf("%b\n", i);  
+    printf("%d\n", bitcount_naive(i));  
+    printf("%d\n", bitcount_kerningham(i));  
+    printf("------\n");
+  }
 
   return 0;
 }
