b3c2fb8bff75ba7d461880278c676148f4644dcd
radio487 <137423799+radio487@users.noreply.github.com>
2026-01-04 16:00:57 +0100

    Lookup table version added for the bit counting program.

--- bit_count.c
+++ bit_count.c
@@ -1,14 +1,21 @@
 /*
- * Two variants. The first iterates all the bits by comparing with 1, and right shifting each round. Scales linearly with the amount of bits in the int.
+ * Three variants. The first iterates all the bits by comparing with 1, and right shifting each round. Scales linearly with the amount of bits in the int.
+ *
  * The second is from Brian Kerningham (it can be found in a K&R exercise) and uses a clever way to set the least significant non-zero bit to zero to abort the loop in a way that scales linearly with the number of set bits.
+ *
+ * The third is a lookup table that trades speed for memory. I will compute the table with Kerningham's algorithm
  */
 
 #include <stdio.h>
+#include <stdint.h>
 
-#define SIZE 20
+#define TEST_INTS 40
+#define TABLE_SIZE 256
 
-int bitcount_naive(unsigned int a) {
-  int c = 0;
+static uint8_t lookup_table[TABLE_SIZE];
+
+uint8_t bitcount_naive(uint32_t a) {
+  uint8_t c = 0;
   while (a) {
     if (a & 1) {
       c++;
@@ -17,20 +24,37 @@ int bitcount_naive(unsigned int a) {
   }
   return c;
 }
-int bitcount_kerningham(unsigned int a) {
-  int c = 0;
+int bitcount_kerningham(uint32_t a) {
+  uint8_t c = 0;
   while (a) {
     c += 1;
     a &= a - 1;
   }
   return c;
 }
+void compute_table(uint8_t *t) {
+  for (int i = 0; i < TABLE_SIZE; ++i) {
+    t[i] = bitcount_kerningham(i);
+  }
+}
+int bitcount_table(uint32_t a) {
+  uint8_t c = 0;
+  while (a) {
+    c += lookup_table[a & 0xFF]; 
+    a >>= 8;
+  }
+  return c;
+}
 
 int main() {
-  for (int i = 0; i < SIZE; ++i) {
+  
+  compute_table(lookup_table);
+
+  for (int i = 0; i < TEST_INTS; ++i) {
     printf("%b\n", i);  
     printf("%d\n", bitcount_naive(i));  
     printf("%d\n", bitcount_kerningham(i));  
+    printf("%d\n", bitcount_table(i));  
     printf("------\n");
   }
 
